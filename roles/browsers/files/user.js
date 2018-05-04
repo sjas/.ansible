f0489ba59bb8355befc13d0caf71ef3a0551aa6a
sjas <teilzeittresor@web.de>
2018-05-04 02:08:15 +0200

    fix firefox default settings deployment ... hopefully

---
+++ roles/browsers/files/user.js
@@ -0,0 +1,8 @@
+// no bookmarks
+user_pref("browser.chrome.site_icons", false);
+// start with previous tabs
+user_pref("browser.startup.page", 3);
+// proper scrollspeed
+user_pref("mousewheel.default.delta_multiplier_y", 300);
+// disable any cachings
+user_pref("browser.cache.disk.parent_directory", '/dev/null');
