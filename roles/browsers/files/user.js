7bd502bca0da331c42435ea589352c0737622725
sjas <teilzeittresor@web.de>
2018-10-06 23:52:48 +0200

    add firefox theme

--- roles/browsers/files/user.js
+++ roles/browsers/files/user.js
@@ -6,3 +6,9 @@ user_pref("browser.startup.page", 3);
 user_pref("mousewheel.default.delta_multiplier_y", 450);
 // disable caching
 user_pref("browser.cache.disk.parent_directory", '/dev/null');
+
+// tron theme
+user_pref("lightweightThemes.persisted.headerURL", true);
+user_pref("lightweightThemes.persisted.footerURL", true);
+user_pref("lightweightThemes.selectedThemeID", "265427");
+user_pref("lightweightThemes.usedThemes", "[{\"id\":\"265427\",\"name\":\"Tron Legacy Beau Garrett\",\"headerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/h_tron1.jpg?modified=060a68f9\",\"footerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/f_tron1.jpg?modified=060a68f9\",\"textcolor\":\"#bdebfc\",\"accentcolor\":\"#0c1c2b\",\"iconURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_small.jpg?modified=060a68f9\",\"previewURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_large.jpg?modified=060a68f9\",\"author\":\"EatingStick\",\"description\":\"Beau Garrett in TRON Legacy. Movie, Disney, blue, black, pretty, sexy\",\"updateURL\":\"https://versioncheck.addons.mozilla.org/en-US/themes/update-check/265427\",\"version\":\"1.0\",\"updateDate\":1538861699460,\"installDate\":1538861699460}]");
