a728ed4e7b535a183e1de71b61185ea3cd32d473
sjas <dummyschliessfach@web.de>
2023-01-16 01:22:20 +0100

    ff scrolling

--- roles/0x06_browsers/files/user.js
+++ roles/0x06_browsers/files/user.js
@@ -3,14 +3,14 @@
 // start with previous tabs
 user_pref("browser.startup.page", 3);
 // proper scrollspeed
-user_pref("mousewheel.default.delta_multiplier_y", 550);
+user_pref("mousewheel.default.delta_multiplier_y", 200);
 // disable caching
 user_pref("browser.cache.disk.parent_directory", '/dev/null');
 // scroll tabs by mousewheel
-user_pref("toolkit.tabbox.switchByScrolling", true);
+user_pref("toolkit.tabbox.switchByScrolling", false);
 
 // tron theme
-user_pref("lightweightThemes.persisted.headerURL", true);
-user_pref("lightweightThemes.persisted.footerURL", true);
-user_pref("lightweightThemes.selectedThemeID", "265427");
-user_pref("lightweightThemes.usedThemes", "[{\"id\":\"265427\",\"name\":\"Tron Legacy Beau Garrett\",\"headerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/h_tron1.jpg?modified=060a68f9\",\"footerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/f_tron1.jpg?modified=060a68f9\",\"textcolor\":\"#bdebfc\",\"accentcolor\":\"#0c1c2b\",\"iconURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_small.jpg?modified=060a68f9\",\"previewURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_large.jpg?modified=060a68f9\",\"author\":\"EatingStick\",\"description\":\"Beau Garrett in TRON Legacy. Movie, Disney, blue, black, pretty, sexy\",\"updateURL\":\"https://versioncheck.addons.mozilla.org/en-US/themes/update-check/265427\",\"version\":\"1.0\",\"updateDate\":1538861699460,\"installDate\":1538861699460}]");
+//user_pref("lightweightThemes.persisted.headerURL", true);
+//user_pref("lightweightThemes.persisted.footerURL", true);
+//user_pref("lightweightThemes.selectedThemeID", "265427");
+//user_pref("lightweightThemes.usedThemes", "[{\"id\":\"265427\",\"name\":\"Tron Legacy Beau Garrett\",\"headerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/h_tron1.jpg?modified=060a68f9\",\"footerURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/f_tron1.jpg?modified=060a68f9\",\"textcolor\":\"#bdebfc\",\"accentcolor\":\"#0c1c2b\",\"iconURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_small.jpg?modified=060a68f9\",\"previewURL\":\"https://addons.cdn.mozilla.net/user-media/addons/265427/preview_large.jpg?modified=060a68f9\",\"author\":\"EatingStick\",\"description\":\"Beau Garrett in TRON Legacy. Movie, Disney, blue, black, pretty, sexy\",\"updateURL\":\"https://versioncheck.addons.mozilla.org/en-US/themes/update-check/265427\",\"version\":\"1.0\",\"updateDate\":1538861699460,\"installDate\":1538861699460}]");
